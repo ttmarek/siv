343d7b326a36c6a0cc41f9e225d5da9e0175511e
ttmarek <ttmarek@outlook.com>
2016-02-18 21:33:21 -0700

    Updated navigateImages to take different parameters

--- app/navigateImages.js
+++ app/navigateImages.js
@@ -2,13 +2,11 @@ module.exports = navigateImages
 
 /**
  * @param {string} direction either "next" or "prev"
- * @param {object} store redux store with list of paths and the current path
+ * @param {string} currentImg path of the current image
+ * @param {array} pathsList list of paths to navigate
  * @returns {string} the next or previous image path
  */
-function navigateImages (direction, store) {
-  const state = store.getState()
-  const currentImg = state.currentImg
-  const pathsList = state.filePaths.pathsList
+function navigateImages (direction, currentImg, pathsList) {
   if (pathsList.length > 0) {
     const currIndex = pathsList.indexOf(currentImg)
     if (currIndex === -1) {
