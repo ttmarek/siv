a54d5eaaa8cf9564493c9f6356ff5eefa4d59aac
Thomas Marek <ttmarek@outlook.com>
2016-07-10 20:56:23 -0400

    Refactor file-box-file and its tests

--- test/file-box-file.js
+++ test/file-box-file.js
@@ -1,38 +1,28 @@
 'use strict'
 const test = require('tape')
 const React = require('react')
-const DOMServer = require('react-dom/server')
-const FileBoxFile = require('../file-box-file')
-
-const render = (reactElement) => {
-  return DOMServer.renderToStaticMarkup(reactElement)
-}
+const { shallow } = require('enzyme')
+const h = require('react-hyperscript');
+const FileBoxFile = require('../app/file-box-file')
 
 test('component/file-box-file', assert => {
-  let file
-  // Inactive image
-  file = render(
-    React.createElement(
-      FileBoxFile,
-      {
-        path: '/path/to/img1.jpg',
-        currentImg: '/path/to/img2.jpg'
-      }
-    )
-  )
-  assert.equal(file,
-               '<li><a href="" class="" data-file-path="/path/to/img1.jpg">img1</a></li>')
-  // Active image
-  file = render(
-    React.createElement(
-      FileBoxFile,
-      {
-        path: '/path/to/img10.jpg',
-        currentImg: '/path/to/img10.jpg'
-      }
-    )
-  )
-  assert.equal(file,
-               '<li><a href="" class="active" data-file-path="/path/to/img10.jpg">img10</a></li>')
-  assert.end()
+  assert.plan(2)
+  let element
+  let expected
+
+  element = h(FileBoxFile, {
+    path: '/path/to/img1.jpg',
+    currentImg: '/path/to/img2.jpg',
+  })
+  expected = '<li><a href="" class="" data-file-path="/path/to/img1.jpg">img1</a></li>'
+  assert.equal(shallow(element).html(), expected,
+               `Renders inactive image links like so: ${expected}`)
+
+  element = h(FileBoxFile, {
+    path: '/path/to/img10.jpg',
+    currentImg: '/path/to/img10.jpg',
+  })
+  expected = '<li><a href="" class="active" data-file-path="/path/to/img10.jpg">img10</a></li>'
+  assert.equal(shallow(element).html(), expected,
+               `Renders active image links like so: ${expected}`)
 })
